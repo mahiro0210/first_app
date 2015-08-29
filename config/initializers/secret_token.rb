7ba669ea1e0bd4bafe3dd927b2a3cc8942702428
Hiroshi Makino <info@xflow.jpn.com>
2015-08-29 14:14:12 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a046f72a4b7e2e324625ede555543127f8984b6f4f3956262e733a34943bdd06e5ef06611ff3191f238e6f21aed926a1cf20e855ad8fd7e93c1e500383c58325'
